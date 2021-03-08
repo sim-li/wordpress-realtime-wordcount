d71be7feeb7b9d84368bd34a250b04baaeeed9ef
Simon Lischka <simon@a-studios.org>
2021-03-08 10:01:41 +0100

    Correct package name that lead to conf not beeing read
    Popped up with a CORS exception in FE

--- bff/src/main/kotlin/xyz/lischka/bff/infrastructure/config/socket/WebsocketConfig.kt
+++ bff/src/main/kotlin/xyz/lischka/bff/infrastructure/config/socket/WebsocketConfig.kt
@@ -1,4 +1,4 @@
-package xyz.lischka.bff.infrastructure.config
+package xyz.lischka.bff.infrastructure.config.socket
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
